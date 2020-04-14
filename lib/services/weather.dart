cec311ee6e4c3d20e48dfce2b2df764185a666a4
Scott Ellis <ellis.lowell.scott@gmail.com>
2020-04-14 11:06:58 -0700

    completed project

--- lib/services/weather.dart
+++ lib/services/weather.dart
@@ -1,4 +1,31 @@
+import '../services/location.dart';
+import '../services/networking.dart';
+import 'networking.dart';
+
+const apiKey = 'c7e61523feef2e8095a865e7d2999232';
+const openWeatherMapURL = 'https://api.openweathermap.org/data/2.5/weather';
+
 class WeatherModel {
+  Future<dynamic> getCityWeather(String cityName) async {
+    var url = '$openWeatherMapURL?q=$cityName&appid=$apiKey&units=imperial';
+    NetworkHelper networkHelper = NetworkHelper(url);
+
+    var weatherData = await networkHelper.getData();
+    return weatherData;
+  }
+
+  Future<dynamic> getLocationWeather() async {
+    Location location = Location();
+    await location.getCurrentLocation();
+
+    NetworkHelper networkHelper = NetworkHelper(
+        '$openWeatherMapURL?lat=${location.latitude}&lon=${location.longitude}&appid=$apiKey&units=imperial');
+
+    var weatherData = await networkHelper.getData();
+
+    return weatherData;
+  }
+
   String getWeatherIcon(int condition) {
     if (condition < 300) {
       return '🌩';
@@ -20,11 +47,11 @@ class WeatherModel {
   }
 
   String getMessage(int temp) {
-    if (temp > 25) {
+    if (temp > 80) {
       return 'It\'s 🍦 time';
-    } else if (temp > 20) {
+    } else if (temp > 60) {
       return 'Time for shorts and 👕';
-    } else if (temp < 10) {
+    } else if (temp <= 59) {
       return 'You\'ll need 🧣 and 🧤';
     } else {
       return 'Bring a 🧥 just in case';
